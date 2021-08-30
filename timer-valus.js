eb467dfb4153ad7743d013147f77330cbd02aa5e
gosnrl <gosnrl51@gmail.com>
2021-08-31 07:33:54 +0900

    Create timer-valus.js

---
+++ timer-valus.js
@@ -0,0 +1,3 @@
+
+var def = {"via":"web","name":"Custom Timer","type":0,"numberOfSets":1,"soundScheme":8,"intervals":[{"name":"Interval 1","duration":3,"color":0,"$$hashKey":"object:3"},{"name":"Interval 2","duration":3,"color":0,"$$hashKey":"object:9"},{"name":"Interval 3","duration":3,"color":0,"$$hashKey":"object:19"},{"name":"Interval 4","duration":3,"color":0,"$$hashKey":"object:33"}]};
+  
